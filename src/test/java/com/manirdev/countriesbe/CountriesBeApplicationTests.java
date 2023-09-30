ffc975e498ceaf7c23a819c3afe77fa7268f51df
manirDev <mhtnourmhtmjr@gmail.com>
2023-09-30 11:39:02 +0300

    project architecture done

---
+++ src/test/java/com/manirdev/countriesbe/CountriesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manirdev.countriesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
